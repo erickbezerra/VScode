53e39b3d06c399a795677c6da85b24949f42939b
erickbezerra <erick.bezerra@gmail.com>
2023-01-16 10:22:16 -0300

    NF

--- teste.py
+++ teste.py
@@ -1,10 +1,25 @@
-def solution(s):
-    if (len(s)/2.0).is_integer():
-        return [s[i:i + 2] for i in range(0, len(s), 2)]
-    else:
-        return [s[i:i + 2].ljust(2, "_") for i in range(0, len(s), 2)]
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
-k = 2 # number of characteres
-s = "ericka"
+structure = [4, 10, 1]
 
-solution(s)
+fig, ax = plt.subplots()
+
+for i, layer in enumerate(structure):
+    for j in range(layer):
+        ax.add_artist(plt.Circle((i, j), radius=0.35, fc='w'))
+        ax.text(i, j, f'{i}, {j}', ha='center', va='center')
+
+for i, layer in enumerate(structure[:-1]):
+    for j in range(layer):
+        for k in range(structure[i+1]):
+            ax.add_line(Line2D([i+0.5, i+1.5], [j, k]))
+
+ax.set_xlim(-0.5, len(structure)-0.5)
+ax.set_ylim(-0.5, max(structure)-0.5)
+ax.set_xticks(range(len(structure)))
+ax.set_yticks(range(max(structure)))
+ax.set_xlabel('Layer')
+ax.set_ylabel('Node')
+plt.show()
